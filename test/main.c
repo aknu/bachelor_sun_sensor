f639147704ca6dc084fbd2e8d03d194ab010f9d2
aknu <andreasknutli@gmail.com>
2021-02-12 13:05:31 +0100

    Add files via upload

---
+++ test/main.c
@@ -0,0 +1,18 @@
+/*
+ * test.c
+ *
+ * Created: 12.02.2021 12:57:31
+ * Author : andre
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
