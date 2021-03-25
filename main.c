5c7b3cfa6c00e3fd79945348e8ef4d0e8fdb8b20
aknu <andreasknutli@gmail.com>
2021-03-25 21:16:44 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -5,28 +5,25 @@
  * Author : andre
  */ 
 
-#define F_CPU 4e6
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <math.h>
-#include <util/delay.h>
 #include "ADC.h"
 #include "I2C_slave.h"
 
 uint8_t slaveAddress = 0x08;
 uint8_t cmd;
 
-int32_t A = 10;
-int32_t B = 5;
-int32_t C = 50;
-int32_t D = 100;
+int32_t A = 5;
+int32_t B = 20;
+int32_t C = 100;
+int32_t D = 20;
 
 float h = 2.18e-3;
 float r = 1.26e-3;
 
-double x;
-double y;
+float x;
+float y;
 
 float phi;
 float theta;
@@ -39,24 +36,26 @@ ISR(TWI0_TWIS_vect){
 	{						
 		I2C_sendAck();													//Send ACK
 	}
-	else if(TWI0_SSTATUS & TWI_DIF_bm)																//Data interrupt
+	else if(TWI0_SSTATUS & TWI_DIF_bm)									//Data interrupt
 	{
-		
 		if(TWI0.SSTATUS & TWI_DIR_bm)									//Master read from slave
 		{					
+			//I2C_sendData(cmd,theta_i2c,phi_i2c);
+			
 			switch(cmd)
 			{
 				case 0x00:
-					TWI0.SDATA = (theta_i2c >> 8) & 0xFF;					//theta MSB
+					PORTC.OUT = 100;
+					TWI0.SDATA = (theta_i2c >> 8) & 0xFF;			//theta MSB
 					break;
 				case 0x01:
-					TWI0.SDATA = (theta_i2c >> 0) & 0xFF;					//theta LSB
+					TWI0.SDATA = (theta_i2c >> 0) & 0xFF;			//theta LSB
 					break;
 				case 0x02:
-					TWI0.SDATA = (phi_i2c >> 8) & 0xFF;						//phi MSB
+					TWI0.SDATA = (phi_i2c >> 8) & 0xFF;				//phi MSB
 					break;
 				case 0x03:
-					TWI0.SDATA = (phi_i2c >> 0) & 0xFF;						//phi LSB
+					TWI0.SDATA = (phi_i2c >> 0) & 0xFF;				//phi LSB
 					break;
 				default:
 					TWI0.SDATA = 111;
@@ -77,8 +76,7 @@ ISR(TWI0_TWIS_vect){
 					x = (float)(B+C-A-D)/(A+B+C+D)*r;
 					y = (float)(A+B-C-D)/(A+B+C+D)*r;
 					
-					
-					phi = atan(sqrt(pow(x,2)+pow(y,2))/h)*180/M_PI;
+					phi = atan((float)sqrt(pow(x,2)+pow(y,2))/h)*180/M_PI;
 
 					if(x < 0)											//-x,+y and -x,-y
 					{
@@ -89,17 +87,21 @@ ISR(TWI0_TWIS_vect){
 					{
 						if(y < 0)										//+x,-y
 						{
+							PORTC.OUT = 2;
 							theta = atan(y/x)*180/M_PI*-1;
 						}
 						else if(y > 0)									//+x,+y
 						{
+							PORTC.OUT = 4;
 							theta = atan(y/x)*180/M_PI*-1+360;
 						}
 						else											//x=y=0
 						{
+							PORTC.OUT = 8;
 							theta = 0;
 						}
 					}
+					break;
 				phi_i2c = (int)round((phi*100));
 				theta_i2c = (int)round((theta*100));
 			}
